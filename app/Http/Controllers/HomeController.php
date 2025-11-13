6da83d1b393ff3fb16bf2391a03da58624dcfb45
Fardizza Vinda Rahman <fardizzar7@gmail.com>
2025-11-13 15:47:57 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        // memanggil semua data yang ada di tabel
+        $users = User::all();
+
+        return view('welcome', compact('users'));
+    }
+    public function show($id)
+    {
+        $user = User::findorFail($id);
+
+        return view('user-detail', compact('user'));
+    }
+
 }
